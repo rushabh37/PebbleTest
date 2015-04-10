b9372c07ba14a1bee550006768e24ab1d0ead3b5
rushabh37 <rushabh37@gmail.com>
2015-04-10 14:03:53 -0400

    Converting time update to minutes from seconds
    Seconds seemed like it consumed lot of watch power

--- src/main.c
+++ src/main.c
@@ -32,15 +32,16 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "00:00:00";
+  static char buffer[] = "00:00";
 
   // Write the current hours and minutes into the buffer
+  // Putting Seconds give pretty bad battery performance
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00:00"), "%H:%M:%S", tick_time);
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00:00"), "%I:%M:%S", tick_time);
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
 
   // Display this time on the TextLayer
@@ -61,12 +62,12 @@ static void main_window_load(Window *window) {
   
   // Create GFont
   // Resource ID is prefixed
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TIME_FONT_28));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TIME_FONT_34));
   
   
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(10, 70, 144, 50));
+  s_time_layer = text_layer_create(GRect(0, 68, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
 
@@ -74,7 +75,7 @@ static void main_window_load(Window *window) {
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
   // text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   
@@ -145,7 +146,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
   
   // Get weather update every 30 minutes
-  if(tick_time->tm_min % 30 == 0 && tick_time->tm_sec == 0) {
+  if(tick_time->tm_min % 30 == 0) {
     // Begin dictionary
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
@@ -219,7 +220,8 @@ static void init() {
   battery_state_service_subscribe(battery_handler);
   
   // Register with TickTimerService
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  // SECOND_UNIT uses lot of watch power hence we use MINUTE_UNIT
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
