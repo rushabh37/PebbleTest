af449578e5d0cc290ec8cbd07ba5b875b6c60f2e
rushabh37 <rushabh37@gmail.com>
2016-02-14 18:58:25 -0500

    Updating as per cloud version

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
 #define KEY_CITY 2
-#define str(x) #x
 #include <pebble.h>
 
 static char context_char = 'c';
@@ -23,6 +22,26 @@ static char temperature_buffer[8];
 static char conditions_buffer[32];
 static char weather_layer_buffer[32];
 static char city_layer_buffer[32];
+
+static char *translate_error(AppMessageResult result) {
+  switch (result) {
+    case APP_MSG_OK: return "APP_MSG_OK";
+    case APP_MSG_SEND_TIMEOUT: return "APP_MSG_SEND_TIMEOUT";
+    case APP_MSG_SEND_REJECTED: return "APP_MSG_SEND_REJECTED";
+    case APP_MSG_NOT_CONNECTED: return "APP_MSG_NOT_CONNECTED";
+    case APP_MSG_APP_NOT_RUNNING: return "APP_MSG_APP_NOT_RUNNING";
+    case APP_MSG_INVALID_ARGS: return "APP_MSG_INVALID_ARGS";
+    case APP_MSG_BUSY: return "APP_MSG_BUSY";
+    case APP_MSG_BUFFER_OVERFLOW: return "APP_MSG_BUFFER_OVERFLOW";
+    case APP_MSG_ALREADY_RELEASED: return "APP_MSG_ALREADY_RELEASED";
+    case APP_MSG_CALLBACK_ALREADY_REGISTERED: return "APP_MSG_CALLBACK_ALREADY_REGISTERED";
+    case APP_MSG_CALLBACK_NOT_REGISTERED: return "APP_MSG_CALLBACK_NOT_REGISTERED";
+    case APP_MSG_OUT_OF_MEMORY: return "APP_MSG_OUT_OF_MEMORY";
+    case APP_MSG_CLOSED: return "APP_MSG_CLOSED";
+    case APP_MSG_INTERNAL_ERROR: return "APP_MSG_INTERNAL_ERROR";
+    default: return "UNKNOWN ERROR";
+  }
+}
 
 static void battery_handler(BatteryChargeState new_state) {
   // Write to buffer and display
@@ -201,7 +220,9 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
   APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-  APP_LOG(APP_LOG_LEVEL_ERROR, str(reason));
+  char reason_str[100];
+  snprintf(reason_str, 100, "%s", translate_error(reason));
+  APP_LOG(APP_LOG_LEVEL_ERROR, reason_str);
 }
 
 static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
